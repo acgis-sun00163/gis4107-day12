ba0c177c7b2ca7826b3b93a9372b3e2548065b70
sun00163 <sun00163@algonquinlive.com>
2019-11-26 14:32:12 -0500

    This is the first push, one more try  !!!

---
+++ exercise_template.py
@@ -0,0 +1,27 @@
+# -*- coding: UTF-8 -*-
+# ------------------------------------------------------------------------------
+# Name:        exercise_template.py
+#
+# Purpose:     Brief desciption of what module does
+#
+# Usage:       Module name and required/optional command-line parameters (if any)
+#
+# Author:      Your name(s)
+#
+# Created:     dd/mm/yyyy
+# ------------------------------------------------------------------------------
+
+def main():
+    pass
+
+def func(params):
+    """Function documentation:
+       - What does function do?
+       - What is/are expected parameter value(s)?
+       - What does function return, if anything
+       - Example usage"""
+
+    pass
+
+if __name__ == '__main__':
+    main()
